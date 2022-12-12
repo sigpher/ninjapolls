0a8a512d16a3bbab38b0ae91b7b89b789c3718d2
sigpher <425223341@qq.com>
2022-12-12 10:40:09 +0800

    first commit

---
+++ src/App.svelte
@@ -0,0 +1,62 @@
+<script>
+	import Header from "./components/Header.svelte";
+	import Footer from "./components/Footer.svelte";
+	import PollList from "./components/PollList.svelte";
+	import CreatePollForm from "./components/CreatePollForm.svelte";
+	import Tabs from "./shared/Tabs.svelte";
+
+	let items = ["Current Polls", "Add New Poll"];
+	let activeItem = "Current Polls";
+	const tabChange = (e) => {
+		activeItem = e.detail;
+	};
+	//已经转移到store
+	// let polls = [
+	// 	{
+	// 		id: 1,
+	// 		question: "Python or JavaScript?",
+	// 		answerA: "Python",
+	// 		answerB: "JavaScript",
+	// 		votesA: 9,
+	// 		votesB: 15,
+	// 	},
+	// ];
+
+	const handleAdd = (e) => {
+		// const poll = e.detail;
+		// polls = [poll, ...polls];
+		// console.log(polls);
+		activeItem = "Current Polls";
+	};
+
+	// const handleVote = (e) => {
+
+	// };
+
+	// import { tweened } from "svelte/motion";
+	// const value = tweened(0);
+</script>
+
+<!-- <button
+	on:click={() => {value.set(1);
+	}}>{$value}</button
+> -->
+
+<Header />
+
+<main>
+	<Tabs {activeItem} {items} on:tabChange={tabChange} />
+	{#if activeItem === "Current Polls"}
+		<PollList />
+	{:else if activeItem === "Add New Poll"}
+		<CreatePollForm on:add={handleAdd} />
+	{/if}
+</main>
+<Footer />
+
+<style>
+	main {
+		max-width: 960px;
+		margin: 40px auto;
+	}
+</style>
---
+++ src/components/PollList.svelte
@@ -0,0 +1,38 @@
+<script>
+    import { fade, slide, scale } from "svelte/transition";
+    import { flip } from "svelte/animate";
+    // import { onDestroy, onMount } from "svelte";
+    import PollStore from "../stores/PollStore";
+    import PollDetails from "./PollDetails.svelte";
+
+    //用$PollStore代替以下这么多行
+    // export let polls = [];
+    // const unsub = PollStore.subscribe((data) => {
+    //     polls = data;
+    // });
+
+    // onMount(() => {
+    //     console.log("component mounted");
+    // });
+    // onDestroy(() => {
+    //     console.log("component destroy");
+    //     unsub();
+    // });
+</script>
+
+<div class="poll-list">
+    {#each $PollStore as poll (poll.id)}
+        <!-- <div in:fade out:slide> -->
+        <div in:fade out:scale|local animate:flip={{ duration: 500 }}>
+            <PollDetails {poll} />
+        </div>
+    {/each}
+</div>
+
+<style>
+    .poll-list {
+        display: grid;
+        grid-template-columns: 1fr 1fr;
+        grid-gap: 20px;
+    }
+</style>
